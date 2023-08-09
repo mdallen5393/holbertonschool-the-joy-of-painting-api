4c31c72296f209a42b30574bb69151d0eee15a84
mdallen5393 <allen_matthew@outlook.com>
2023-08-09 08:53:47 -0500

    many changes; wow, this is a useful commit message; im so burnt out

--- create.sql
+++ create.sql
@@ -2,45 +2,76 @@ DROP DATABASE IF EXISTS tjopdb;
 CREATE DATABASE tjopdb;
 USE tjopdb;
 
-CREATE TABLE episodes (
-    episode_id INT PRIMARY KEY,
+CREATE TABLE Colors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    hex VARCHAR(7) NOT NULL
+);
+
+CREATE TABLE Episodes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
     season INT,
     episode INT,
-    title VARCHAR(100),
-    youtube_src VARCHAR(500),
+    youtube_src VARCHAR(255),
+    img_src VARCHAR(255),   -- Include img_src directly in the Episodes table
     air_date DATE
 );
 
-CREATE TABLE subjects (
-    subject_id INT PRIMARY KEY,
-    name VARCHAR(100)
+CREATE TABLE Subjects (
+    episode_id INT,
+    subject_name VARCHAR(255),
+    PRIMARY KEY (episode_id, subject_name),
+    FOREIGN KEY (episode_id) REFERENCES Episodes(id)
 );
 
-CREATE TABLE colors (
-    color_id INT PRIMARY KEY,
-    name VARCHAR(100)
-);
-
-CREATE TABLE paintings (
-    painting_id INT PRIMARY KEY,
-    img_src VARCHAR(250),
-    title VARCHAR(100),
-    episode_id INT UNIQUE,
-    FOREIGN KEY (episode_id) REFERENCES episodes (episode_id)
-);
-
-CREATE TABLE painting_colors (
-    painting_color_id INT PRIMARY KEY AUTO_INCREMENT,
-    painting_id INT,
+CREATE TABLE PaintingColors (
     color_id INT,
-    FOREIGN KEY (painting_id) REFERENCES paintings (painting_id),
-    FOREIGN KEY (color_id) REFERENCES colors (color_id)
+    episode_id INT,
+    PRIMARY KEY (color_id, episode_id), -- Change primary key to use episode_id
+    FOREIGN KEY (color_id) REFERENCES Colors(id),
+    FOREIGN KEY (episode_id) REFERENCES Episodes(id)
 );
 
-CREATE TABLE painting_subjects (
-    painting_subject_id INT PRIMARY KEY AUTO_INCREMENT,
-    painting_id INT,
-    subject_id INT,
-    FOREIGN KEY (painting_id) REFERENCES paintings (painting_id),
-    FOREIGN KEY (subject_id) REFERENCES subjects (subject_id)
-);
+
+-- DROP DATABASE IF EXISTS tjopdb;
+-- CREATE DATABASE tjopdb;
+-- USE tjopdb;
+
+-- CREATE TABLE Colors (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255),
+--     hex VARCHAR(7) NOT NULL
+-- );
+
+-- CREATE TABLE Episodes (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(255),
+--     season INT,
+--     episode INT,
+--     youtube_src VARCHAR(255),
+--     air_date DATE
+-- );
+
+-- CREATE TABLE Paintings (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(255),
+--     img_src VARCHAR(255),
+--     episode_id INT,
+--     FOREIGN KEY (episode_id) REFERENCES Episodes(id)
+-- );
+
+-- CREATE TABLE Subjects (
+--     episode_id INT,
+--     subject_name VARCHAR(255),
+--     PRIMARY KEY (episode_id, subject_name),
+--     FOREIGN KEY (episode_id) REFERENCES Episodes(id)
+-- );
+
+-- CREATE TABLE PaintingColors (
+--     color_id INT,
+--     painting_id INT,
+--     PRIMARY KEY (color_id, painting_id),
+--     FOREIGN KEY (color_id) REFERENCES Colors(id),
+--     FOREIGN KEY (painting_id) REFERENCES Paintings(id)
+-- );
